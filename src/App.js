b2e533d3fec98b63b58b539c6f544db48fdbd7fb
budimbu esuku <budimbuesu@gmail.com>
2024-08-25 06:13:07 +0200

    Tailwind installed and test OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <p>HELLO WORLD </p>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h3 className="text-3xl font-bold text-teal-500">Hello Tailwind</h3>
+    </div>
+  );
 }
 
 export default App;
